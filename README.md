fa5198a2faf323533f2cb30948516b6fb493ef56
Ngoc Son <ngocson19102002@gmail.com>
2024-09-04 14:44:33 +0700

    push code

---
+++ README.md
@@ -0,0 +1,21 @@
+## Một số lệnh git cơ bản:
+    ```
+    - git status
+    - git add . 
+    - git commit -m"noi dung comment"
+    - git push origin ten-nhanh
+    ```
+## Tạo tên nhánh:
+    ```
+    "git checkout -b feature/tenhocvien/ten-tinh-nang" - Đối với tính năng mới
+    "git checkout -b hotfix/tenhocvien/ten-tinh-nang" - Đối với update tính năng
+    ```
+## Nhánh temp
+    ```
+    - "admin-temp"
+    ```
+## Nhánh Reactjs
+    ```
+    - dev
+    - sit
+    ```
---
+++ dist/js/snike.js
@@ -0,0 +1,25 @@
+$('.product-list').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
